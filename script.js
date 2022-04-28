ea12ce9a63add588999f3bd98b254c873147485f
MohammadHajMahmoud <mohammdhasaeen@yahoo.com>
2022-04-28 21:23:59 +0300

    Update script.js
    trying fetch

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-let firstReq = new XMLHttpRequest();
- firstReq.addEventListener("load",function() {
-      let data = JSON.parse(firstReq.responseText).articles;
-         showPost (data); 
-});
- firstReq.open ("GET", "https://newsapi.org/v2/top-headlines?country=eg&category=sports&apiKey=58f3c1a6b6184fc48e7e4da47112e798" ) ;
-  firstReq.send();
+async function get(){
+  let response = await fetch("https://newsapi.org/v2/top-headlines?country=eg&category=sports&apiKey=58f3c1a6b6184fc48e7e4da47112e798")
+  let data =await response.json();
+  showPost(data.articles);
+}
 
+get();
 
 
 function showPost(data){
-     let htm1Data = document.querySelector(".data");
-      let toHtml="";
-       data.forEach(function (d) { 
-           if(toHtml.length<2400)
+  
+    let htm1Data = document.querySelector(".data");
+    let toHtml="";
+     data.forEach(function (d) { 
+         
 toHtml +=`
 <div class="card" >
 <div class="card-header">
-  <img src="${d.urlToImage}"<img>
+<img src="${d.urlToImage}"<img>
 </div>
 <div class="card-body">
-  <p>${d.title}
-  </p>
-  <a href="${d.url}" class="btn">Read more</a>
+<p>${d.title}
+</p>
+<a href="${d.url}" class="btn">Read more</a>
 </div></div>
-   ` 
-
-
-;
-       })
-       htm1Data.innerHTML = toHtml;
-    }     
+ `;
+     });
+     htm1Data.innerHTML = toHtml;
+  }
+   
+    
 
     
     
